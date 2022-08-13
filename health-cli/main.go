e060ca22a4f422c5b8f97484dd7f4c2db0d987e2
Michael Alls <allsjr@gmail.com>
2022-08-13 12:07:35 -0400

    update iport path

--- health-cli/main.go
+++ health-cli/main.go
@@ -4,7 +4,7 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/mallsjr/health-cli/cmd"
+import "github.com/mallsjr/health-cli/health-cli/cmd"
 
 func main() {
 	cmd.Execute()
