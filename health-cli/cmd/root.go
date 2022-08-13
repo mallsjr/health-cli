4ee3fade17a26a32f2339acf1296261a6c6b1d0d
Michael Alls <allsjr@gmail.com>
2022-08-13 17:53:35 -0400

    cleaned up root.go

--- health-cli/cmd/root.go
+++ health-cli/cmd/root.go
@@ -10,21 +10,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "health-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A CLI for health check of REST services",
+	Long: `A CLI for health check of REST services.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -45,7 +35,7 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 
